70103246ca5a5b6825efc9a063520c7a5c81b7c8
Anjar Fiandrianto <anjar@akvo.org>
2023-09-01 13:10:42 +0700

    Add node operation

---
+++ helpers/node/node-operation.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euv
+
+# ${1} for node version
+# ${2} for npm command
+
+docker run \
+       --rm \
+       --volume "$(pwd):/app" \
+       --workdir "/app" \
+       --entrypoint /bin/sh \
+       akvo/akvo-node-18-alpine:20230831.105309.b9593b7 -c "${2}"
